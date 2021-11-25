4d711aa54dd28e3bfd34fd8efc959ec8b4ee4ce5
adam <adamdupuis@laposte.net>
2021-11-25 13:46:32 +0100

    message controller user verification added

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const MessageModel = require("../model/message.model");
-const UserModel = require("../model/user.model")
+const DiscussionModel = require("../model/discussion.model")
 const {param, validationResult} = require("express-validator");
 const router = express.Router()
 const i18n = require('../i18n.config')
 const {maxMessagesDisplayed} = require("../config")
-const myHelper = require('../helpers/helper')
 
 /**
  * @namespace messageController
@@ -16,14 +15,19 @@ const myHelper = require('../helpers/helper')
  * @memberof messageController
  */
 router.post('/', async (req, res) => {
-    myHelper.isUserLogged(req, res)
-    try{
-        let message = new MessageModel(req.body)
-        message = await message.save()
-        res.status(201).send({message: message})
-    } catch(e) {
-        res.status(400).send(e)
+    let message = new MessageModel(req.body)
+    let discussion = await DiscussionModel.findOne({_id: req.body.discussion})
+    if (!discussion.members.includes(req.body.author)){
+        return res.status(401).send({message: i18n.t('unauthorized')})
+    } else {
+        try{
+            message = await message.save()
+            res.status(201).send({message: message})
+        } catch(e) {
+            res.status(400).send(e)
+        }
     }
+
 })
 
 /**
@@ -47,19 +51,21 @@ router.put('/edit/:id',
         const message = await MessageModel.findOne({_id: req.params.id})
         if (!message) {
             res.status(404).send({message: i18n.t('message not found')})
-        }
-        message.content_history.push(message.content)
-
-        if(req.body.action === "edit"){
-            message.content = req.body.content
-        } else if(req.body.action === "delete"){
-            message.content = i18n.t('Message deleted')
+        } else if (message.author._id !== req.user._id) {
+            return res.status(401).send({message: i18n.t('unauthorized')})
         } else {
-            return res.status(404).send({message: i18n.t('action doesn\'t exist')})
-        }
+            message.content_history.push(message.content)
+            if (req.body.action === "edit") {
+                message.content = req.body.content
+            } else if (req.body.action === "delete") {
+                message.content = i18n.t('Message deleted')
+            } else {
+                return res.status(404).send({message: i18n.t('action doesn\'t exist')})
+            }
 
-        message.save()
-        res.send({message})
+            message.save()
+            res.send({message})
+        }
     }
 )
 
@@ -81,21 +87,25 @@ router.get('/:id',
         next()
     },
     async (req, res) => {
-        if (req.body.page === 0) {
-            req.body.page = 1
+        const discussion = await DiscussionModel.findOne({id: req.params.id})
+        if (!discussion.members.includes(req.user._id)){
+            return res.status(401).send({message: i18n.t('unauthorized')})
+        } else {
+            if (req.body.page === 0) {
+                req.body.page = 1
+            }
+            let page_number = req.body.page
+            let offset = maxMessagesDisplayed * page_number - maxMessagesDisplayed
+            const messages = await MessageModel
+                .find({discussion: req.params.id})
+                .sort({'createdAt': 1})
+                .skip(offset)
+                .limit(maxMessagesDisplayed)
+            if (messages.length === 0) {
+                return res.status(404).send({message: i18n.t('No message to display')})
+            }
+            res.send({messages})
         }
-        let page_number = req.body.page
-        let offset = maxMessagesDisplayed * page_number - maxMessagesDisplayed
-        const messages = await MessageModel
-            .find({discussion: req.params.id})
-            .sort({'createdAt': 1})
-            .skip(offset)
-            .limit(maxMessagesDisplayed)
-        myHelper.isUserLogged(req, res)
-        if (messages.length === 0){
-            return res.status(404).send({message: i18n.t('No message to display')})
-        }
-        res.send({messages})
     })
 
 module.exports = router
